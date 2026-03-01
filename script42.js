1fd0f2427180a1e48a679299299ab54e99448769
harshal1516 <mohiteharshal22@gmail.com>
2026-03-01 19:39:37 +0530

    Add initial HTML structure, CSS styles, and JavaScript for DOM manipulation

---
+++ script42.js
@@ -0,0 +1,11 @@
+/* DOM - Document Object Model is a programming interface for web documents. It represents the page so that programs can change the document structure
+FRONT END KI JS
+DOM 4 PILLARS
+SELECTION OF ELEMENTS
+CHANGIG html
+CHANGING CSS
+EVENTS LISTENERS */
+
+ var h1 = document.querySelector("h1")
+ var h2 = document.querySelector("h2")
+ h1.innerHTML= "Hello World"
