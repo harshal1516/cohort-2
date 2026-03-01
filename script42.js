884d398e21fcc4fec9f003a3a5c27548b8511c8b
harshal1516 <mohiteharshal22@gmail.com>
2026-03-01 19:44:14 +0530

    Add DOM manipulation examples with header styling and content updates

--- script42.js
+++ script42.js
@@ -8,4 +8,11 @@ EVENTS LISTENERS */
 
  var h1 = document.querySelector("h1")
  var h2 = document.querySelector("h2")
- h1.innerHTML= "Hello World"
+ h1.innerHTML= "Hello World" // This will change the content of h1 to "Hello World"
+ h2.innerHTML= "Welcome to DOM Manipulation" // This will change the content of h2 to "Welcome to DOM Manipulation"
+ 
+
+ h1.style.color = "red" // This will change the color of h1 to red
+ h1.style.backgroundColor = "yellow" // This will change the background color of h1 to yellow
+ h2.style.color = "blue" // This will change the color of h2 to blue
+ h2.style.backgroundColor = "lightgray" // This will change the background color of h2 to light gray
