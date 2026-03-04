5fb81c7098de8900b35de7e132af05db4f2681e2
harshal1516 <mohiteharshal22@gmail.com>
2026-03-04 11:07:47 +0530

    ARRAY OBJECT IN JS

--- script43.js
+++ script43.js
@@ -1,4 +1,51 @@
-var a =Math.floor (Math.random()*100)
-console.log(a);
+// var btn=document.querySelector('button')
+// var box= document.querySelector('#box')
 
 
+// btn.addEventListener('click',function(){
+//     // console.log("hello");
+//      var c1 = Math.floor(Math.random()*256)
+//     var c2 = Math.floor(Math.random()*256)
+//     var c3 = Math.floor(Math.random()*256)
+//     // console.log(c1,c2,c3);
+    
+// box.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+    
+
+// })
+
+// var arr =['vedant','Harshal', 'Abhi','Manik','Bhatt','Emu']
+
+// var a= Math.floor(Math.random()*arr.length)
+
+// console.log(arr[a]);
+
+var arr = [
+    {
+        team:'CSK',
+        primary:'yellow',
+        secondary:'blue',
+
+    },
+    {
+        team:'RCB',
+        primary:'Red',
+        secondary:'Black',
+
+
+    },
+    {
+        team:'MI',
+        primary:'Blue',
+        secondary:'Gold',
+
+    },
+    
+    {
+        team:'KKR',
+        primary:'Purple',
+        secondary:'Gold',
+
+    }
+]
+console.log(arr[2].team);
