85ecdbc4172baf6122a7c7ba2a65679409a688c5
harshal1516 <mohiteharshal22@gmail.com>
2026-03-06 12:39:15 +0530

    Update button styles and add click event functionality

---
+++ script44.js
@@ -0,0 +1,15 @@
+// var btn = document.querySelector('button')
+// btn.addEventListener('click',function(){
+//    var h1= document.createElement('div')
+//    h1.innerHTML='hello this is  js'
+//     console.log(h1);
+
+// })
+
+var h1 = document.createElement('button')
+h1.innerHTML = 'download'
+h1.style.color='red'
+
+var main = document.querySelector('main')
+
+main.appendChild(h1)
