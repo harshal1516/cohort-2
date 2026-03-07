73128bd8bd93f4824362ae6404425628891a57a0
harshal1516 <mohiteharshal22@gmail.com>
2026-03-07 18:16:44 +0530

    Update button text and implement click event to create a red div

--- script44.js
+++ script44.js
@@ -6,10 +6,32 @@
 
 // })
 
-var h1 = document.createElement('button')
-h1.innerHTML = 'download'
-h1.style.color='red'
+// var h1 = document.createElement('button')
+// h1.innerHTML = 'download'
+// h1.style.color='red'
 
+// var main = document.querySelector('main')
+
+// main.appendChild(h1)
+
+// var div = document.createElement('div')
+
+// div.style.height='100px'
+// div.style.width='100px'
+// div.style.backgroundColor = 'red'
+
+// var main = document.querySelector('main')
+//  main.appendChild(div)
+
+var btn = document.querySelector('button')
 var main = document.querySelector('main')
 
-main.appendChild(h1)
+btn.addEventListener ('click',function(){
+var div = document.createElement('div')   
+div.style.height='50px'
+div.style.width='50px'
+div.style.backgroundColor='red'
+
+console.log(div);
+
+})
