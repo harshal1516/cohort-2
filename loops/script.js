596451a453d383b008e0847ea48a5c5355972550
harshal1516 <mohiteharshal22@gmail.com>
2026-09-09 22:59:49 +0530

    Add initial HTML and JavaScript files for loops project

---
+++ loops/script.js
@@ -0,0 +1,34 @@
+// for (let i =10; i>0;i--){// start ;end;change;{ }
+// console.log(i);
+// }
+
+// now while loop 
+
+// start
+// while (end){
+// code
+// change}
+
+// let i = 10;
+// while(i>0){
+  
+//     console.log(i);
+//       i--
+    
+// }
+
+// for (let i=1; i<21; i++){
+// if (i%2 === 0){
+//     console.log(i);
+    
+// }    
+// }
+
+let i =0
+while(i<15){
+    if (i%2 === 1){
+    console.log(i);
+    }
+    i++
+    
+}
