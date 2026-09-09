b20eaf39bc44a39534669555411c2e9b987676a1
harshal1516 <mohiteharshal22@gmail.com>
2026-09-09 23:00:48 +0530

    Refactor while loop implementation in script.js for improved readability

--- loops/script.js
+++ loops/script.js
@@ -9,20 +9,20 @@
 // code
 // change}
 
-// let i = 10;
-// while(i>0){
+let i = 10;
+while(i>0){
   
-//     console.log(i);
-//       i--
-    
-// }
-
-for (let i=1; i<21; i++){
-if (i%2 === 0){
     console.log(i);
+      i--
     
-}    
 }
+
+// for (let i=1; i<21; i++){
+// if (i%2 === 0){
+//     console.log(i);
+    
+// }    
+// }
 
 // let i =0
 // while(i<15){
