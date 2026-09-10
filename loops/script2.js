72d97ddf3f6f0a7176ea0a19d4c65caea29d937d
harshal1516 <mohiteharshal22@gmail.com>
2026-09-10 22:06:14 +0530

    count keyword in loops

--- loops/script2.js
+++ loops/script2.js
@@ -1,7 +1,21 @@
-for (let i=1;i<21;i++){
+// for (let i=1;i<21;i++){
   
     
-    if (i%3===0) continue// this will skip the if condition and will run the next line.
+//     if (i%3===0) continue// this will skip the if condition and will run the next line.
    
-      console.log(i);
+//       console.log(i);
+// }
+
+// count keyword
+
+let count = 0;
+
+for (i=1;i<101;i++){
+    if(i%2 ===1){
+        count++;
+        console.log(i);
+        
+        
+    }
+    if (count===5)break
 }
