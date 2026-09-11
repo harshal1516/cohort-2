968f5b8150be9e01e660236542be8b3fcd2cc358
harshal1516 <mohiteharshal22@gmail.com>
2026-09-11 11:45:56 +0530

    parametes and arguments in js

---
+++ function in js/script.js
@@ -0,0 +1,20 @@
+// fecthing dynamic values from function 
+// function dance (v1){
+//     console.log(`${v1} nach rha hai`);
+// }
+
+// dance("ghoda");
+// dance("mor");
+// dance("sher");
+// dance("gadha");
+
+
+// parameters and arguments in js
+
+function add(v1,v2){ //parameters fucntion word k brackets m jo hai woh
+    console.log(v1+v2);
+
+    
+}
+
+add(1,2);//arguments mtlb jaha hm real values bhej rhe hai woh
